4663078949b55e31a7893260420d1fdb12debef0
Myles Abbott <myles.abbott@rea-group.com>
2015-03-17 16:24:28 +1100

    Clean up boilerplate code

--- mPlayground/AppDelegate.h
+++ mPlayground/AppDelegate.h
@@ -1,9 +1,4 @@
 //
-//  AppDelegate.h
-//  mPlayground
-//
-//  Created by Myles Abbott on 1/10/2014.
-//  Copyright (c) 2014 Myles Abbott. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- mPlayground/ViewController.h
+++ mPlayground/ViewController.h
@@ -1,15 +1,9 @@
 //
-//  ViewController.h
-//  mPlayground
-//
-//  Created by Myles Abbott on 1/10/2014.
-//  Copyright (c) 2014 Myles Abbott. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
 
 @end
 
