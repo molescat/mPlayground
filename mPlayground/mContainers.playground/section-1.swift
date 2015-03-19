7cbeb73c358669740464379a220297b32c2ecd8d
Myles Abbott <myles.abbott@rea-group.com>
2015-03-19 18:23:07 +1100

    Add a simple generic method to print out the type of the container

--- mPlayground/mContainers.playground/section-1.swift
+++ mPlayground/mContainers.playground/section-1.swift
@@ -1,21 +1,29 @@
-// Playground - noun: a place where people can play
+// Playground - Containers
+// Arrays, Dictionaries
 
 import UIKit
 
 var mString = "Hello, world"
 
+func printMyType<T>(myType: T) {
+  println("TypeName = \(_stdlib_getTypeName(myType))")
+}
+
 // ---------------------------------------- Dictionary
-var mEmptyDictionary = [:]
+let mEmptyDictionary = [:]
+printMyType(mEmptyDictionary)
+
 var mDict = ["apple":1, "banana":2]
 mDict["orange"] = 3
 mDict
 
 
 // ---------------------------------------- Array
-var mEmptyArray = []
+let mEmptyArray = []
+printMyType(mEmptyArray)
+
 var mArray = ["apple", "banana"]
 mString = mArray[1]
 mArray.append("cherry")
 mArray += ["orange", "lemon"]
-
 
