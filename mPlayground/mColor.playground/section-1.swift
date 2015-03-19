0998dc7d5cb116f10bf2a93b05d0e3faa2364689
Myles Abbott <myles.abbott@rea-group.com>
2015-03-19 17:32:01 +1100

    Add a simple UIKit color

---
+++ mPlayground/mColor.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground 
+// Color examples
+
+import UIKit
+
+var apple = UIColor.greenColor()
+
+let ball = UIColor(red: (253.0/255.0), green: (159.0/255.0), blue: (47.0/255.0), alpha: 1.0)
+
+let box = UIColor.blackColor().colorWithAlphaComponent(0.5)
+
+
