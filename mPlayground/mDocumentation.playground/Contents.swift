58dcc8ccc4d7ab93a5d30515d958bf028008e69e
Myles Abbott <myles.abbott@rea-group.com>
2015-04-14 16:37:55 +1000

    Add MarkDown playground examples

---
+++ mPlayground/mDocumentation.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - Documentation
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//: This is a line of text that will appear as a rich comment in a playground.
+
+
+//: These two lines of single delimiter text
+//: show up as one rich comment block in the playground
+
+
+
+/*: A comment that is not displayed in rich text view
+# The First Heading Shown
+The first content line displayed in this block of rich text.
+*/
+
+
+/*: A bulleted list of 2 items
+* Item 1
+* Item 2
+*/
