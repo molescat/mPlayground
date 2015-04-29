7966c5737d3a9a83921824c03a985cf1dd1b2aaa
Myles Abbott <myles.abbott@rea-group.com>
2015-04-29 18:27:10 +1000

    Associated values - enum

--- mPlayground/mEnumeration.playground/Contents.swift
+++ mPlayground/mEnumeration.playground/Contents.swift
@@ -9,8 +9,12 @@ enum CompassPoint {
   case West
 }
 
-enum Planet {
-  case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+var direction = CompassPoint.North
+
+
+//--------------------------------- RawValue value
+enum Planet: Int {
+  case Mercury = 1, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
 }
 
 var planet = Planet.Mercury
@@ -25,3 +29,33 @@ default: // switch must be exhaustive
   println("just a rock")
 }
 
+let possiblePlanet = Planet(rawValue: 7)
+let impossiblePlanet = Planet(rawValue: 14)
+
+
+//--------------------------------- Associated value
+enum Barcode {
+  case UPCA(Int, Int, Int, Int)
+  case QRCode(String)
+}
+
+var item = Barcode.UPCA(8, 85909, 51226, 3)
+
+item = Barcode.QRCode("1234567890")
+
+switch item {
+case .UPCA(let numberSystem, let manufacturer, let product, let check):
+  println("UPCA-A: \(numberSystem), \(manufacturer), \(product), \(check)")
+case let .QRCode(productCode):
+  println("QR Code: \(productCode)")
+}
+
+
+//--------------------------------- Prepoulated
+enum ASCIIControlChars: Character {
+  case Tab = "\t"
+  case LineFeed = "\n"
+  case CarriageReturn = "\r"
+}
+
+println("test\(ASCIIControlChars.Tab.rawValue)test")
