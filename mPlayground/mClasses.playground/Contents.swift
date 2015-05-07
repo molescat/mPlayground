e68706586aa3cc1c5673748ee73e0305f8a78bb5
Myles Abbott <myles.abbott@rea-group.com>
2015-05-07 17:30:17 +1000

    Type Properties

--- mPlayground/mClasses.playground/Contents.swift
+++ mPlayground/mClasses.playground/Contents.swift
@@ -58,3 +58,44 @@ manager.data.append("Some data")
 manager.data.append("Some more data")
 // the DataImporter instance for the importer property has not yet been created
 
+
+// ---------------------------------------------- Type Properties
+class SomeClass {
+  static var storedTypeProperty = "Some value."
+  static var computedTypeProperty: Int {
+    return 23
+  }
+  class var overrideableComputedTypeProperty: Int {
+    return 24
+  }
+  class func someTypeMethod() {
+    println("hello class type method")
+  }
+}
+
+SomeClass.storedTypeProperty
+SomeClass.storedTypeProperty = "frog"
+SomeClass.storedTypeProperty
+SomeClass.someTypeMethod()
+
+
+// ----------------------------------------------
+
+class MyPoint {
+  var x = 0.0, y = 0.0
+  init() {}
+  init(x: Double, y :Double) { self.x = x; self.y = y }
+
+  func mutatingMoveByX(deltaX: Double, y deltaY: Double) {
+    x += deltaX
+    y += deltaY
+  }
+  func moveByX(deltaX: Double, y deltaY: Double) -> MyPoint {
+    return MyPoint(x: x + deltaX, y: y + deltaY)
+  }
+}
+var defaultPoint = MyPoint()
+var somePoint = MyPoint(x: 1.0, y: 1.0)
+somePoint.mutatingMoveByX(2.0, y: 3.0)
+
+var anotherPoint = somePoint.moveByX(3.0, y: 4.0)
--- mPlayground/mStruct.playground/Contents.swift
+++ mPlayground/mStruct.playground/Contents.swift
@@ -82,3 +82,34 @@ stepCounter.totalSteps = 200
 
   To see the above output of the willSet, didSet - check the console
 */
+
+
+// -------------------- Type Properties
+struct SomeStructure {
+  static var storedTypeProperty = "Some value."
+  static var computedTypeProperty: Int {
+    return 23
+  }
+  static func someTypeMethod() {
+    println("Hello some Type Method")
+  }
+}
+SomeStructure.storedTypeProperty
+SomeStructure.someTypeMethod()
+
+
+// -------------------- Functions
+struct MyPoint {
+  var x = 0.0, y = 0.0
+  mutating func mutatingMoveByX(deltaX: Double, y deltaY: Double) {
+    x += deltaX
+    y += deltaY
+  }
+  func moveByX(deltaX: Double, y deltaY: Double) -> MyPoint {
+    return MyPoint(x: x + deltaX, y: y + deltaY)
+  }
+}
+var somePoint = MyPoint(x: 1.0, y: 1.0)
+somePoint.mutatingMoveByX(2.0, y: 3.0)
+
+var anotherPoint = somePoint.moveByX(3.0, y: 4.0)
