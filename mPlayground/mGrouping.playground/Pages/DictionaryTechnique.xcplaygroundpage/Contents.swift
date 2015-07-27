5115426c1ca6aca6080942e86b4027540c9f4437
Myles Abbott <myles.abbott@rea-group.com>
2015-07-28 09:15:22 +1000

    Add grouping example

---
+++ mPlayground/mGrouping.playground/Pages/DictionaryTechnique.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+//: [Previous](@previous)
+
+//: Grouping - simple experiment using dictionary as the unique key
+
+import Foundation
+
+extension Array {
+  func groupBy <U> (groupingFunction group: (Element) -> U) -> [U: Array] {
+    var result = [U: Array]()
+    for item in self {
+      let groupKey = group(item)
+      // If element has already been added to dictionary, append to it. If not, create one.
+      if result[groupKey] != nil {
+        result[groupKey]! += [item]
+      } else {
+        result[groupKey] = [item]
+      }
+    }
+    
+    return result
+  }
+}
+
+func groupForAddress(address: String) -> Int {
+  let addrs = ["a":1, "b":2, "c":1, "d":3, "e":3, "f":1, "g":4, "h":5, "i":4]
+  return addrs[address]!
+}
+
+
+let keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+
+let groups = keys.groupBy(groupingFunction: groupForAddress)
+
+
+
+//: [Next](@next)
---
+++ mPlayground/mGrouping.playground/Pages/GroupingCoordinateListings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: Grouping - using dictionary as the unique key
+
+import UIKit
+
+class Listing : CustomStringConvertible {
+  let latitude: Int!
+  let name: String!
+  init(latitude: Int, name: String) {self.latitude = latitude; self.name = name }
+  
+  func makeKey() -> String {
+    return String(self.latitude)
+  }
+
+  var description: String { return "\(name) at \(latitude)" }
+}
+
+let testListing = Listing(latitude: 0, name: "Test")
+testListing.makeKey()
+
+let listings:[Listing] = [
+  Listing(latitude: 1, name: "Alice"),
+  Listing(latitude: 2, name: "Bob"),
+  Listing(latitude: 3, name: "Charles"),
+  Listing(latitude: 1, name: "David"),
+  Listing(latitude: 4, name: "Eric"),
+  Listing(latitude: 2, name: "Frank")
+]
+
+extension Array {
+  func groupBy <U> (groupingFunction group: (Element) -> U) -> [U: Array] {
+    var result = [U: Array]()
+    for item in self {
+      let groupKey = group(item)
+      // If element has already been added to dictionary, append to it. If not, create one.
+      if result[groupKey] != nil {
+        result[groupKey]! += [item]
+      } else {
+        result[groupKey] = [item]
+      }
+    }
+    
+    return result
+  }
+}
+
+func groupForAddress(listing: Listing) -> String {
+  return listing.makeKey()
+}
+
+let groups = listings.groupBy(groupingFunction: groupForAddress)
+
+for (key, value) in groups {
+  print("Key '\(key)' Value '\(value)'")
+}
+
+//: [Next](@next)
