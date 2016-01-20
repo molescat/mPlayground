f903f937a4fccf107878ec5034b636ddf3dcab69
Myles Abbott <myles.abbott@rea-group.com>
2016-01-21 09:29:11 +1100

    Closure shorthand progression

--- mPlayground/mFunctions.playground/Pages/Closure.xcplaygroundpage/Contents.swift
+++ mPlayground/mFunctions.playground/Pages/Closure.xcplaygroundpage/Contents.swift
@@ -2,8 +2,9 @@
 
 import Foundation
 
-// ---------------------------------------- Closure (sort, map)
 var strangeNumbers = [10, 100, 8, 16]
+
+// ---------------------------------------- map
 
 let doubledNumbers = strangeNumbers.map({
   (number: Int) -> Int in
@@ -17,6 +18,7 @@ let trippledNumbers = strangeNumbers.map({ number in
 trippledNumbers
 
 
+// ----------------------------------------- sort
 func backwards(a :Int, b :Int) -> Bool {
   return a > b
 }
@@ -25,18 +27,52 @@ let reverseNumbers = strangeNumbers.sort(backwards)
 let sortedNumbers = strangeNumbers.sort{ $0 > $1 }
 sortedNumbers
 
-strangeNumbers.sortInPlace(<)
+strangeNumbers.sort(<)
+strangeNumbers
+
+strangeNumbers.sortInPlace(<)  // Mutate
+strangeNumbers
 
 
-// ---------------------------------------- Trailing Closure
+// ---------------------------------------- Trailing Closure shorthand
 func someFunctionThatTakesAClosure(closure: () -> ()) {
   closure()
 }
 
-someFunctionThatTakesAClosure({print("without using trailing closure")})
+someFunctionThatTakesAClosure({
+  print("without using trailing closure")
+})
 
 someFunctionThatTakesAClosure() {
   print("Using trailing closure")
+}
+
+
+// ---
+var names = ["Steve", "Bill", "Woz", "Stew", "Myles", "Rie"]
+
+let sNames = names.filter({ (name: String) -> Bool in
+  return name.lowercaseString.characters.first! == "s"
+})
+
+let sNames2 = names.filter() { name -> Bool in
+  return name.lowercaseString.characters.first! == "s"
+}
+
+let sNames3 = names.filter { name -> Bool in
+  return name.lowercaseString.characters.first! == "s"
+}
+
+let sNames4 = names.filter { name in
+  return name.lowercaseString.characters.first! == "s"
+}
+
+let sNames5 = names.filter { name in
+  name.lowercaseString.characters.first! == "s"
+}
+
+let sNames6 = names.filter {
+  $0.lowercaseString.characters.first! == "s"
 }
 
 //: [Next](@next)
