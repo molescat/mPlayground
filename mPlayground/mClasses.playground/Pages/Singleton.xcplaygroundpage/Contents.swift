ea3a2ada44a3aa3cc553032e4a9a82d9520e32ee
Myles Abbott <myles.abbott@rea-group.com>
2016-01-25 09:05:29 +1100

    Add SimpleSingleton

---
+++ mPlayground/mClasses.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Note the init is private
+//let mySingleton = SimpleSingleton()
+
+let mySingletonA = SimpleSingleton.sharedInstance
+mySingletonA.title = "A"
+
+let mySingletonB = SimpleSingleton.sharedInstance
+mySingletonB.title = "B"
+
+print(mySingletonA.title)
+print(mySingletonB.title)
+
+//: [Next](@next)
---
+++ mPlayground/mClasses.playground/Sources/SimpleSingleton.swift
@@ -0,0 +1,7 @@
+/// Access shared instance via `sharedInstance` type property, e.g., `SimpleSingleton.sharedInstance`
+public class SimpleSingleton {
+  public static let sharedInstance = SimpleSingleton()
+  public var title = ""
+  
+  private init() { } // private is the default in a playground
+}
