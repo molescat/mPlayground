4042dbd25463b1809bbd254b1cf5c898b54d43a4
Myles Abbott <myles.abbott@rea-group.com>
2016-02-11 10:15:53 +1100

    Sets

---
+++ mPlayground/mSet.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+struct Option: OptionSetType {
+  
+  let rawValue: Int
+  
+  init(rawValue: Int) {
+    self.rawValue = rawValue
+  }
+  
+  static let A = Option(rawValue: 1 << 0)
+  static let B = Option(rawValue: 1 << 1)
+  static let C = Option(rawValue: 1 << 2)
+  
+  static let AAndC = [A, C]
+  static let All = [A, B, C]
+  
+}
+
+let allOptions: Option = [.A, .B, .C]
+print(allOptions)
+
+
+let all = Option.All
+
+for option in all {
+  print(option)
+}
+
+//----
+var letters = Set<Character>()
+print("letters is of type Set<Character> with \(letters.count) items.")
+
+letters.insert("a")
+letters.insert("b")
+letters.count
+
+letters = []
+// letters is now an empty set, but is still of type Set<Character>
+
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+var favoriteGenresShort: Set = ["Rock", "Classical", "Hip hop"]
+
+favoriteGenres.insert(")
