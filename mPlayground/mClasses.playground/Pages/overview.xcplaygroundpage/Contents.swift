d39081f0e3683ed8d0b961525f99ecbe727375e2
Myles Abbott <myles.abbott@rea-group.com>
2016-02-24 11:35:27 +1100

    Small update to class - Doc header

---
+++ mPlayground/mClasses.playground/Pages/overview.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+/*
+Classes have additional capabilities that structures do not:
+
+* Inheritance enables one class to inherit the characteristics of another.
+* Type casting enables you to check and interpret the type of a class instance at runtime.
+* Deinitializers enable an instance of a class to free up any resources it has assigned.
+* Reference counting allows more than one reference to a class instance.
+
+*/
+
+//: [Next](@next)
