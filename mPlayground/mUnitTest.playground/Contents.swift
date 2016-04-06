8d0ca39b512a7ef4610536d8d1855e36e2475b18
Myles Abbott <myles.abbott@rea-group.com>
2016-04-07 09:01:44 +1000

    Add the start of the UnitTest Adapter Pattern

---
+++ mPlayground/mUnitTest.playground/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+## Unit Test - Stubbing and mocking
+*/
+
+import Foundation
+
+protocol TabBarToggle {
+  static func shouldShowTabBar() -> Bool
+}
+
+class REAToggle {
+  //: ... other code goes here
+}
+
+//: ---
+
+extension REAToggle: TabBarToggle {
+  static func shouldShowTabBar() -> Bool {
+    return true
+  }
+}
+
+//: ---
+
+class FakeToggleToHideTabBar: TabBarToggle {
+  static func shouldShowTabBar() -> Bool {
+    return false
+  }
+}
+
