47c4d2ac7d672dfbd02dbc83e0c6e8233efb91e7
Myles Abbott <myles.abbott@hotmail.com>
2016-05-28 15:16:53 +1000

    Prep for SwiftDojo. Templated Binding.

--- mPlayground/mSwiftDojo.playground/Pages/BasicViewController.xcplaygroundpage/Contents.swift
+++ mPlayground/mSwiftDojo.playground/Pages/BasicViewController.xcplaygroundpage/Contents.swift
@@ -1,73 +1,35 @@
 //: [Previous](@previous)
 /*:
- ## Templated Binding Object
+ ## SwiftDojo - Basic ViewController
  */
 
 import UIKit
 import XCPlayground
 
-
-class Binding<T> {
-  typealias Listener = T -> Void
-  var listener: Listener?
-  
-  var value: T {
-    didSet {
-      listener?(value)
-    }
-  }
-  
-  init(_ value: T)
-  {
-    self.value = value
-  }
-  
-  func bind(listener: Listener?) {
-    self.listener = listener
-    listener?(value)
-  }
-}
-
-class ViewModel {
-  var counter: Binding<Int> = Binding(0)
-  
-  func incrementCounter() {
-    counter.value += 5
-  }
-  
-  func bindCounterLabel(label: UILabel) {
-    counter.bind { [weak label] in
-      label?.text = "Counter \($0)"
-    }
-  }
-}
-
 class MyViewController: UIViewController {
-  
-  var viewModel = ViewModel()
   
   var textLabel = UILabel(frame: CGRect(x: 10, y: 10, width: 300, height: 44))
   
   override func viewDidLoad() {
     view.backgroundColor = UIColor.whiteColor()
     
-    textLabel.backgroundColor = UIColor.whiteColor()
+    textLabel.backgroundColor = UIColor.lightGrayColor()
     view.addSubview(textLabel)
-
+    
     let button = UIButton(type: UIButtonType.System)
     button.frame = CGRect(x: 10, y: 60, width: 300, height: 44)
     button.setTitle("Update", forState: UIControlState.Normal)
     button.backgroundColor = UIColor.cyanColor()
     button.addTarget(self, action: #selector(MyViewController.buttonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
     view.addSubview(button)
-    
-    viewModel.bindCounterLabel(textLabel)
   }
   
   func buttonAction(sender:UIButton!) {
-    viewModel.incrementCounter()
+    print("buttonAction")
   }
   
 }
 
 XCPlaygroundPage.currentPage.liveView = MyViewController()
+
+//: [Next](@next)
--- mPlayground/mSwiftDojo.playground/Pages/WithTemplatedBinding.xcplaygroundpage/Contents.swift
+++ mPlayground/mSwiftDojo.playground/Pages/WithTemplatedBinding.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: [Previous](@previous)
 /*:
- ## Templated Binding Object
+ ## SwiftDojo - Basic ViewController, with ViewModel and Template Binding
  */
 
 import UIKit
@@ -28,6 +28,9 @@ class Binding<T> {
   }
 }
 
+//: ---
+
+
 class ViewModel {
   var counter: Binding<Int> = Binding(0)
   
@@ -35,39 +38,45 @@ class ViewModel {
     counter.value += 5
   }
   
-  func bindCounterLabel(label: UILabel) {
-    counter.bind { [weak label] in
-      label?.text = "Counter \($0)"
-    }
-  }
 }
 
 class MyViewController: UIViewController {
   
-  var viewModel = ViewModel()
-  
   var textLabel = UILabel(frame: CGRect(x: 10, y: 10, width: 300, height: 44))
+  
+  var viewModel = ViewModel()
   
   override func viewDidLoad() {
     view.backgroundColor = UIColor.whiteColor()
     
-    textLabel.backgroundColor = UIColor.whiteColor()
+    textLabel.backgroundColor = UIColor.lightGrayColor()
     view.addSubview(textLabel)
-
+    
     let button = UIButton(type: UIButtonType.System)
     button.frame = CGRect(x: 10, y: 60, width: 300, height: 44)
     button.setTitle("Update", forState: UIControlState.Normal)
     button.backgroundColor = UIColor.cyanColor()
     button.addTarget(self, action: #selector(MyViewController.buttonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
     view.addSubview(button)
-    
-    viewModel.bindCounterLabel(textLabel)
+
+  
+    viewModel.counter.bind { [weak self] in
+      self?.textLabel.text = "Counter \($0)"
+    }
   }
   
   func buttonAction(sender:UIButton!) {
     viewModel.incrementCounter()
+    
+    print("buttonAction, \(viewModel.counter)")
   }
   
 }
 
 XCPlaygroundPage.currentPage.liveView = MyViewController()
+
+//: [Next](@next)
+
+
+
+//: [Next](@next)
