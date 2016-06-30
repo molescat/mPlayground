25ea15878e6b99eeeef216b9f4f5b6fb40a6664e
Myles Abbott <myles.abbott@rea-group.com>
2016-07-01 09:05:22 +1000

    Convert AppDelegate to Swift

---
+++ mPlayground/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2016 Myles Abbott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+}
