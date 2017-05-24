523f36185d85288350cf1756c0644bc50869c663
Myles Abbott <myles.abbott@rea-group.com>
2017-05-24 14:01:40 +1000

    Static var for struct

---
+++ mPlayground/mStruct.playground/Pages/static.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct SearchRefinement {
+  static let clearAllSelectionKey = "search.clearall"
+}
+
+SearchRefinement.clearAllSelectionKey
+
+//: [Next](@next)
